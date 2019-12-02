5181632961e2f374a3207ba433358b764016980c
Peter-Chensl <58041150+Peter-Chensl@users.noreply.github.com>
2019-12-02 22:09:42 +0800

    Add files via upload

---
+++ 作业1/text12.02.0.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int i,j;
+	for (i = 0; i < 8;i ++)
+	{
+	if((i+j)%2==0)
+	printf("%c%c",219,219);
+	else
+	printf(" ");
+	printf("\n");
+	}
+	getch();
+}
