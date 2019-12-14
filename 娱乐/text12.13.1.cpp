afa94771cc90850d935953621baf40ab2fb16dbb
Peter-Chensl <58041150+Peter-Chensl@users.noreply.github.com>
2019-12-14 23:50:09 +0800

    Add files via upload

---
+++ 娱乐/text12.13.1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char c[200],c1;
+	int i, j ,k;
+	printf("ÇëÊäÈëÒ»¶Î×Ö·û£º");
+	scanf("%s",c);
+	k = strlen(c);
+	for(i = 0,j = k - 1;i<k/2;i++,j--)
+	{
+		c1 = c[i];
+		c[i] = c[j];
+		c[j] = c1;
+		printf("%s",c);
+	}
+}
